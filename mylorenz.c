3d1768a33789eedfcd2099d876b4a2cd400e6dd6
DiptarshiC <dich4615@colorado.edu>
2019-09-05 15:24:40 -0600

    created the c that creates the lorentz transform

---
+++ mylorenz.c
@@ -0,0 +1,53 @@
+/**
+*@file: 	mylorenz.c
+*
+*@description:
+*
+*
+*@reference:
+*
+*@author: 	Diptarshi Chakraborty
+*
+*@date: 	05-09-2019
+*
+*/
+
+
+/**
+*@func: 	lorenzgen
+*
+*@description:  a function that simply generates the lorenz co-ordinates and stores them 
+*		in an array
+*
+*@param:	int array[][]
+*
+*@return	void
+*/
+
+void lorenzgen(int arr[][])
+{
+
+
+
+
+
+
+}
+
+/**
+*@func:		main
+*
+*@description:	the main function
+*
+*@param:	int argc, char **argv
+*
+*@return	int
+*/
+
+int main(int argc, char **argv)
+{
+
+
+
+	return 0;
+}
