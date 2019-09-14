8aaf3e561125d59cba60ab4514ef65b2a9d53cb6
DiptarshiC <dich4615@colorado.edu>
2019-09-14 13:43:29 -0600

    Completed the Lorenz Function

--- mylorenz.c
+++ mylorenz.c
@@ -1,8 +1,8 @@
 /**
 *@file: 	mylorenz.c
 *
-*@description:
-*
+*@description:  A C file that contains all the functions that are neccessary to generate a Lorenz
+*		attractor
 *
 *@reference:
 *
@@ -14,44 +14,58 @@
 
 
 /**
-*@func: 	lorenzgen
+*@func: 	gen
 *
-*@description:  a function that simply generates the lorenz co-ordinates and stores them 
-*		in an array
+*@description:  a function that simply generates the lorenz co-ordinates and stores them
+*		in an array. You have to provide array and the number of elements n
 *
-*@param:	int array[][]
 *
-*@return	void
+*@param:	int array[][], int n
 *
-*@todo:		
+*@return:	void
+*
+*@reference:	Referenced from code provided by mr William Schreuder for class CSCI 5229
+*		URL:http://www.prinmath.com/csci5229/misc/lorenz.c
+*
+*@todo:
 */
 
-void lorenzgen(int arr[][])
+void gen(int arr[][],int n, double s, double b, double r)
 {
 
 
+	/*  Coordinates  */
+	double x = 1;
+	double y = 1;
+	double z = 1;
 
+	double delta_x;
+	double delta_y;
+	double delta_z;
+
+	/*Time Step*/
+
+	double delta_t = 0.001;
+
+	for(int i = 0; i < n; i++)
+	{
+
+		delta_x = s*(y - x);
+		delta_y = x*(r - z) -y;
+		delta_z = x*y - b*z;
+
+		x = x + delta_t * delta_x;
+		y = y + delta_t * delta_y;
+		z = z + delta_t * delta_z;
+
+		arr[i][0] = x;
+		arr[i][1] = y;
+		arr[i][2] = z;
+
+	}
 
 
 
 }
 
-/**
-*@func:		main
-*
-*@description:	the main function
-*
-*@param:	int argc, char **argv
-*
-*@return	int
-*
-*@todo:		
-*/
 
-int main(int argc, char **argv)
-{
-
-
-
-	return 0;
-}
